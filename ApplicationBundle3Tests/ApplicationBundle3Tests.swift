3b995563191049c3292e77d3933befdf3f1b6315
FiFiTM <b.f.pabian@gmail.com>
2025-07-11 16:16:55 +0200

    Initial Commit

---
+++ ApplicationBundle3Tests/ApplicationBundle3Tests.swift
@@ -0,0 +1,17 @@
+//
+//  ApplicationBundle3Tests.swift
+//  ApplicationBundle3Tests
+//
+//  Created by Bartosz Pabian on 11/07/2025.
+//
+
+import Testing
+@testable import ApplicationBundle3
+
+struct ApplicationBundle3Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
